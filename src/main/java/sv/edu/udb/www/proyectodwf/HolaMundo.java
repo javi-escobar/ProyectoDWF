392ea7b02e2b3f55c64c82797c60468c98a6287d
javi-escobar <alexispineda2446@gmail.com>
2024-03-17 14:26:31 -0600

    Inicialización del Proyecto

---
+++ src/main/java/sv/edu/udb/www/proyectodwf/HolaMundo.java
@@ -0,0 +1,15 @@
+package sv.edu.udb.www.proyectodwf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HolaMundo {
+    @GetMapping("/")
+    public String HolaMundo() {
+        log.info("Estoy en la clase HolaMundo");
+        return "Hola Mundo";
+    }
+}
